dbb250ae3fc648892f677b12d43fae75b6b1efd2
kiriroN <marykiriro1@gmail.com>
2023-09-21 01:19:32 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,6 +4,8 @@
  * using at most n bytes from src
  * @dest: input value
  * @src: input value
+ * @n: input value
+ *
  * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
@@ -19,9 +21,9 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
 	dest[i] = '\0';
 	return (dest);
