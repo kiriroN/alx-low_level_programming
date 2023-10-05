a70eafee43656e18b373151c536881e6cfab9f98
kiriroN <marykiriro1@gmail.com>
2023-10-05 02:10:16 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (i = 0; s1[i] != '\0'; i++)
 			output[i] = s1[i];
 		for (i = 0; s2[i] != '\0'; i++)
-			output[s1len + 1] = s2[i];
+			output[s1len + i] = s2[i];
 		output[s1len + i] = '\0';
 	}
 	else
