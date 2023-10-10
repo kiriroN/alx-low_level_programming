74448b266b36c5e2b1887e5e8bab9dfeeb364241
kiriroN <marykiriro1@gmail.com>
2023-10-10 00:04:06 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	char age;
+	char *owner;
+};
+#endif
